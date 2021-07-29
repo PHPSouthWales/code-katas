66d6bd5d660347c1362689499e2616ebe1c6588b
Oliver Davies <oliver@oliverdavies.uk>
2021-07-29 23:30:33 +0100

    Add placeholder tests for other katas

---
+++ tests/BowlingTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class BowlingTest extends TestCase
+{
+    /** @test */
+    public function shouldReturnTrue(): void
+    {
+        $this->markTestIncomplete();
+    }
+}
---
+++ tests/RomanNumeralsTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class RomanNumeralsTest extends TestCase
+{
+    /** @test */
+    public function shouldReturnTrue(): void
+    {
+        $this->markTestIncomplete();
+    }
+}
